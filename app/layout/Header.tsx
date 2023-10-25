7452ea83605755ba9f2145aa711cf7894977bcf9
Aaron Enyetu <aaronenyetu@gmail.com>
2023-10-25 16:03:11 -0700

    Completed setting up Auth with Github

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,14 +1,20 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { Button as AuthButton } from "../auth/Button"
+import { SessionProvider} from 'next-auth/react'
 
 type Props = {}
 
 function Header({}: Props) {
-  return (
+    return (
+      <SessionProvider>
       <div className='border-b border-black flex justify-between'>
           <Link href='/' className='text-4xl'>CodeBlog</Link>
-          <button className='text-white bg-slate-600 px-4 py-4 '>Sign In</button>
-    </div>
+                {/* <button className='text-white bg-slate-600 px-4 py-4 '>Sign In</button> */}
+                <AuthButton/>
+            </div>
+            </SessionProvider>
   )
 }
 
