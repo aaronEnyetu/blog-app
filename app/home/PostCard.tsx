13890be6e6927c26f8a6a8182b8166db73e98009
Aaron Enyetu <aaronenyetu@gmail.com>
2023-10-25 13:53:10 -0700

    Added background color to the post cards and made them responsive

--- app/home/PostCard.tsx
+++ app/home/PostCard.tsx
@@ -3,12 +3,13 @@ import type { Post } from '@prisma/client'
 
 type Props = {
     post: Post,
+    className?: string,
 }
 
-const PostCard = ({post}: Props) => {
+const PostCard = ({post, className}: Props) => {
   return (
-      <div>
-          <h3 className='text-3xl mb-3'>{post.title}</h3>
+      <div className={`${className} p-4 rounded border-2 shadow-[0.25rem_0.25rem_0px_rgba(0,0,0,1)]`}>
+          <h3 className='text-2xl lg:text-3xl mb-3'>{post.title}</h3>
           <p>{post.content}</p>
       </div>
   )
--- app/home/Posts.tsx
+++ app/home/Posts.tsx
@@ -7,15 +7,28 @@ const prisma = new PrismaClient()
 type Props = {}
 
 const Posts = async (props: Props) => {
-    const users = await prisma.user.findMany()
-    console.log(users)
+    // const users = await prisma.user.findMany()
+    // console.log(users)
     const posts = await prisma.post.findMany()
-    console.log(posts)
+    // console.log(posts)
+
+    const bgClasses = [
+        'bg-pink-500',
+        'bg-blue-500',
+        'bg-yellow-500',
+        
+    ]
+
+
+
+
+
+
   return (
       <div><h2 className='text-4xl text-center mt-6'>Reflections</h2>
-          <div className='grid grid-cols-3 gap-4 mt-4'>
-              {posts.map((post) => (
-                  <PostCard key={post.id} post={post}/>
+          <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4'>
+              {posts.map((post, index) => (
+                  <PostCard key={post.id} post={post} className={bgClasses[index]} />
               ))}
       </div>
       
