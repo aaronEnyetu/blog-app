df301cca15cb291bf3aa5b1d10faf68d4d84496d
Aaron Enyetu <aaronenyetu@gmail.com>
2023-10-25 11:18:19 -0700

    Created the Hero Section and page

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function Header({}: Props) {
   return (
       <div className='border-b border-black flex justify-between'>
-          <div className='text-4xl'>BLOG</div>
+          <div className='text-4xl'>Life of the Mind</div>
           <button className='text-white bg-slate-600 px-4 py-4 '>Sign In</button>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1300px",
       },
     },
     borderColor: {
